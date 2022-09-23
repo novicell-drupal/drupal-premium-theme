657cbd34b1f117c2e03a5775ab35692ca936612d
Søren Due Bonde <sdb@novicell.dk>
2022-09-23 13:17:25 +0200

    Added javascript file to layout-builder-gin library

---
+++ js/layout-builder-gin.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const bodyElement = document.querySelector('body');
+  bodyElement.setAttribute('style', 'padding-top: 0px !important');
+});
